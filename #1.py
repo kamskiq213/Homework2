b9de135d1026887ff7f2b1b6dd66946f75dbe1e1
kamskiq213 <133587946+kamskiq213@users.noreply.github.com>
2024-09-09 09:56:47 +0500

    Add files via upload

---
+++ #1.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введите 1-ое целое число: "))
+number2 = int(input("Введите 2-ое целое число: "))
+if number1 == number2:
+    print('Числа равны')
+else:
+    print('Числа не равны')
