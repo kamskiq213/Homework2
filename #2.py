a04c17b090857ce5ff9c5fe1714907d20877f727
kamskiq213 <133587946+kamskiq213@users.noreply.github.com>
2024-09-12 14:35:21 +0500

    Delete #2.py

--- #2.py
+++
@@ -1,8 +0,0 @@
-number1 = int(input("Введите делимое число: "))
-number2 = int(input("Введите делитель: "))
-if number2 == 0:
-    print('На 0 делить нельзя')
-elif number1 % number2 == 0:
-    print(f'Число {number1} кратно числу {number2}')
-else:
-    print(f'Число {number1} не кратно числу {number2}')
